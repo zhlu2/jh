849a115a717885fe2beef8381fc5c102a6d053ad
zhlu <503507248@qq.com>
2017-02-21 21:46:14 +0800

    提交

---
+++ webmvc/src/main/java/com/aco/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.aco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "demo";
+    }
+}
